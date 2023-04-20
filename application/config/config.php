9083990b2e0d16e7a54f105f83d358265ed9b79b
himanshu-el <himanshugoyal@externlabs.com>
2023-04-20 15:14:03 +0530

    configuration change

--- application/config/config.php
+++ application/config/config.php
@@ -29,7 +29,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 
-$config['base_url'] = 'http://localhost/ceg_new/';
+// $config['base_url'] = 'http://localhost/ceg_new/';
+$config['base_url'] = 'http://172.22.33.47/';
+
 
 /*
 |--------------------------------------------------------------------------
